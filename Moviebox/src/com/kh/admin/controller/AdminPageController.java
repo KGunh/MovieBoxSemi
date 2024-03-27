363e965209bf1b16506343ecd855a8900b3a65e9
juhee1103 <mm0001@naver.com>
2024-03-27 17:50:05 +0900

    ㅜㅜㅜㅜㅜㅜㅜㅜ

--- Moviebox/src/com/kh/admin/controller/AdminPageController.java
+++ Moviebox/src/com/kh/admin/controller/AdminPageController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.kh.admin.model.service.AdminPageService;
+import com.kh.common.model.vo.PageInfo;
 
 public class AdminPageController {
 	
@@ -28,13 +29,20 @@ public class AdminPageController {
 		pageLimit = 10; //페이징바개수
 		boardLimit = 10; //게시글개수
 		
+		maxPage = (int)Math.ceil((double)listCount / boardLimit);
+		startPage = (currentPage - 1) / pageLimit * pageLimit + 1;
+		endPage = startPage + pageLimit - 1;
+		
+		if(endPage > maxPage) {
+			endPage = maxPage;
+		}
 		
 		
 		
 		
-		
-		
-		
+		//VO로 가공
+		PageInfo pi = new PageInfo(listCount, currentPage, pageLimit, boardLimit,
+				   maxPage, startPage, endPage);
 		//---request.setAttribute로 두개 다 넣기
 		
 		//---view 주소 넣고 리턴
