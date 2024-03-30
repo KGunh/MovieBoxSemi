87f1b31db0117252db8ec78f5aaa092d1e7f0e0d
kirnyuyu <164119090+kirnyuyu@users.noreply.github.com>
2024-03-31 00:24:37 +0900

    ㅠㅠ - 김유나

--- Moviebox/src/com/kh/movie/controller/MovieController.java
+++ Moviebox/src/com/kh/movie/controller/MovieController.java
@@ -1,5 +1,22 @@
 package com.kh.movie.controller;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.kh.movie.model.service.MovieService;
+import com.kh.movie.model.vo.Movie;
+
 public class MovieController {
+	
+	public String selectMovieList(HttpServletRequest request) {
+		
+		ArrayList<Movie> movieList = new MovieService().selectMovieList();
+		request.setAttribute("movieList", movieList);
+		
+		String view = "views/movie/movieList.jsp";
+		
+		return view;
+	}
 
 }
--- Moviebox/src/com/kh/movie/model/service/MovieService.java
+++ Moviebox/src/com/kh/movie/model/service/MovieService.java
@@ -1,5 +1,21 @@
 package com.kh.movie.model.service;
 
+import static com.kh.common.JDBCTemplate.*;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import com.kh.movie.model.dao.MovieDao;
+import com.kh.movie.model.vo.Movie;
+
 public class MovieService {
+
+	public ArrayList<Movie> selectMovieList() {
+		Connection conn = getConnection();
+		ArrayList<Movie> movieList = new MovieDao().selectMovieList(conn);
+		
+		close(conn);
+		return movieList;
+	}
 
 }
