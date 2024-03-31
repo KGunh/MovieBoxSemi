ab3e8ebb70ef5f7286f47790fbfae00841a9f507
juhee1103 <mm0001@naver.com>
2024-03-31 19:21:32 +0900

    커밋

--- Moviebox/src/com/kh/admin/controller/AdminPageController.java
+++ Moviebox/src/com/kh/admin/controller/AdminPageController.java
@@ -175,6 +175,7 @@ public class AdminPageController {
 										 new PosterFileRenamePolicy());
 			//------------------ 파일업로드 ------------------
 			
+			
 			// enctype="multipart/form-data" 는 아래처럼 request를 한번 더 가공 해줬기 때문에
 			// String title = request.getParameter("title");
 			// 이렇게 request로 쓰면 null이 나옴...
@@ -189,64 +190,47 @@ public class AdminPageController {
 	        String story = multiRequest.getParameter("story");
 	        String genre = multiRequest.getParameter("genre");
 	        // 위쪽까지 form 데이터 받기완료
-	        
 	        // 등록정리
 	        // 1. TB_DIRECTOR 테이블에 감독정보가 들어가야함 - 존재할 경우 감독번호(DIRECTOR_NO)만 들고있기
 	        // 2. TB_ACTOR 테이블에 배우 정보가 들어가야함 - 존재할 경우 배우번호(ACTOR_NO)만 들고있기
 	        // 3. TB_MOVIE 테이블에 영화에 대한 정보가 전부 들어가야함 -영화번호(MOVIE_NO)만 들고있기 -> 들고있던 감독번호도 넣어준다.
 	        // 4. TB_CAST 테이블에 캐스팅 정보가 들어가야함 - 들고있던 영화번호, 배우번호 넣기 (LOOP로)
-	        
 	        int directorResult;
 	        int actorResult;
 	        int castResult;
 	        int movieResult;
-	        
 	        // 1.)을 하기위해 감독이름이 한글로 들어오기 때문에, 한글로 DB에 감독 이름 검색
 	        // 기존에 있다면 감독 번호가 나오고, 없다면 0
 	        int directorNo = new AdminPageService().SelectDirectorName(directorName);
-	        
-
 	        if(directorNo == 0) {
 	        	// 0이라면 IF문으로 들어오게되고 감독번호 INSERT하기전에 SEQ를 생성해줘야함 SEQ 생성해주고 directorNo 넣어두자.
 	        	directorNo = new AdminPageService().SelectDirectorNo();
 	        	// 감독 insert
 	        	directorResult = new AdminPageService().InsertDirector(directorNo, directorName);
 	        }
-	        
-	        
 	        // 2.)를 하기 위해서는 출연진은 여러명이기 때문에 콤마를 구분으로 들어오는걸 약속해둬야함!
 	        String[] actorArray = actors.split(",");
-	        
 	        // 최종 TB_CAST테이블에 넣을때 사용할 것
 	        int[] resultActorNo = new int[actorArray.length];
 //	        System.out.println("actorArray length: " + actorArray.length);
-	        
 	        // 배우가 등록되어 있는지 여부를 판단하고, 없으면 넣어줘야함.
 	        for (int i = 0; i < actorArray.length; i++) {
 	        	// trim -> 콤마로 구분시 공백이 들어온다면 공백제거용
 	            actorArray[i] = actorArray[i].trim();
-	            
 		        // 배우 이름이 한글로 들어오기 때문에, 한글로 DB에 배우 이름을 검색하는 로직
 		        // 기존에 있다면 배우 번호가 나오고 없다면 0
 	            int actorNo = new AdminPageService().SelectActorName(actorArray[i]);
-	            
 	            if(actorNo == 0) {
 	            	// 배우 번호 시퀀스를 뽑기
 	            	actorNo = new AdminPageService().SelectActorNo();
-	            	
 	            	// 뽑아준 배우 번호와 입력받은 배우 이름을 TB_ACTOR테이블에 INSERT
 	            	actorResult = new AdminPageService().InsertActor(actorNo, actorArray[i]);
-	            	
 	            }
-	          
 	            resultActorNo[i] = actorNo;
 	        }
-
 	        
 	        // 3.)TB_MOVIE테이블에 INSERT
-
 	        int movieNo = new AdminPageService().SelectMovieNo();
-	        
 	        Movie movie = new Movie();
 	        movie.setMovieNo(movieNo); 
 	        movie.setMovieTitle(title);
@@ -257,27 +241,20 @@ public class AdminPageController {
 	        movie.setGenreNo(genreCode);
 	        movie.setDirectorNo(directorNo);
 	        movieResult = new AdminPageService().InsertMovie(movie);
-	        
-
 	        for (int i = 0; i < resultActorNo.length; i++) {
 	        	castResult = new AdminPageService().InsertCast(movieNo, resultActorNo[i]);
 	        }
-	        
 			String view = "";
-			
 			if(movieResult > 0) {
 				session.setAttribute("alertMsg", "영화가 등록되었습니다.");
 				view = "/adminMovieCheck.admin?currentPage=1";
 			} else {
 				session.setAttribute("alertMsg", "영화가 등록에 실패했습니다.");
-				view = "views/notice/adminMovieInsert.jsp";
+				view = "views/admin/adminMovieInsert.jsp";
 			}
-		
 		}
-		
 		ArrayList<Genre> genrelist = new AdminPageService().SelectGenreList();
 		request.setAttribute("genreList", genrelist);
-		
 		String view = "views/admin/adminMovieInsert.jsp";
 		return view;
 	}
