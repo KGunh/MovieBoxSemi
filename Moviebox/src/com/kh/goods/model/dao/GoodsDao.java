525f433758de299835aa810cdf648092119abd99
kokojj <164119538+kokojj0@users.noreply.github.com>
2024-04-09 16:35:32 +0900

    쫑

--- Moviebox/src/com/kh/goods/model/dao/GoodsDao.java
+++ Moviebox/src/com/kh/goods/model/dao/GoodsDao.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import com.kh.common.model.vo.PageInfo;
 import com.kh.goods.model.vo.Goods;
 
 public class GoodsDao {
@@ -27,29 +28,62 @@ public class GoodsDao {
 			e.printStackTrace();
 		}
 	}
-
-	public List<Goods> selectGoodsList(Connection conn) {
-		List<Goods> goodsList = new ArrayList<Goods>();
+	
+	public int selectListCount(Connection conn) {
+		int listCount = 0;
 		
-		try(PreparedStatement pstmt = conn.prepareStatement(prop.getProperty("selectGoodsList"));
+		try(PreparedStatement pstmt = conn.prepareStatement(prop.getProperty("selectListCount"));
 			ResultSet rset = pstmt.executeQuery();) {
 			
-			while(rset.next()) {
-				Goods goods = new Goods();
-				
-				goods.setChangeName(rset.getString("CHANGE_NAME"));
-				goods.setGoodsName(rset.getString("GOODS_NAME"));
-				goods.setGoodsPrice(rset.getInt("GOODS_PRICE"));
-				goods.setTypeName(rset.getString("TYPE_NAME"));
-				goods.setFilePath(rset.getString("FILE_PATH"));
-				goods.setGoodsNo(rset.getInt("GOODS_NO"));
-				System.out.println(goods.getChangeName()+goods.getFilePath());
-				goodsList.add(goods);
+			rset.next();
+			
+			listCount = rset.getInt("COUNT(*)");
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		
+		return listCount;
+	}
+
+
+	public List<Goods> selectGoodsList(Connection conn, PageInfo pi) {
+		List<Goods> goodsList = new ArrayList<Goods>();
+		
+		try(PreparedStatement pstmt = conn.prepareStatement(prop.getProperty("selectGoodsList"));) {
+			
+			int startRow = (pi.getCurrentPage() - 1) * pi.getBoardLimit() + 1;
+			int endRow = startRow + pi.getBoardLimit() - 1;
+			
+			pstmt.setInt(1, startRow);
+			pstmt.setInt(2, endRow);
+			
+			try(ResultSet rset = pstmt.executeQuery();) {
+			
+				while(rset.next()) {
+					Goods goods = new Goods();
+					
+					goods.setChangeName(rset.getString("CHANGE_NAME"));
+					goods.setGoodsName(rset.getString("GOODS_NAME"));
+					goods.setGoodsPrice(rset.getInt("GOODS_PRICE"));
+					goods.setTypeName(rset.getString("TYPE_NAME"));
+					goods.setFilePath(rset.getString("FILE_PATH"));
+					goods.setGoodsNo(rset.getInt("GOODS_NO"));
+					
+					goodsList.add(goods);
+				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
 		return goodsList;
 	}
 
+	
+	
+	
+	
+	
+	
 }
